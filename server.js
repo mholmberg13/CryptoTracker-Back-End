e60aa2dd1329d0439fe289d2c1bb90b26a204f82
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-13 16:06:48 -0500

    heroku ts

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ mongoose.connection.once('open', ()=> {
 // middleware
 app.use(express.json());
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+
 // Secret for authentication/session:
 app.use(
     session({
